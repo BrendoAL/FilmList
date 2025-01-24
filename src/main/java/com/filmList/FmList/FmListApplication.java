fe65f6a3ab539a69a2b1ed77e221534ed75dc3cc
BrendoXR <114269551+BrendoXR@users.noreply.github.com>
2025-01-24 14:01:10 -0400

    Project created

---
+++ src/main/java/com/filmList/FmList/FmListApplication.java
@@ -0,0 +1,13 @@
+package com.filmList.FmList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filmList/FmList/FmListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmList.FmList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
